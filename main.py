7e0433f0aa52a7d2b5b71074a8bd2b82c1cfeae1
White_mu <WhitemuTeam@outlook.com>
2023-10-14 22:46:30 +0800

    Fix: 完善错误抛出并添加qqbot警告

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from Muice import Muice
 from qqbot import qqbot
-import json,logging,os
+import json,logging,os,traceback
 
 logging.info('在09.10更新中, 将使用(QQ号).json的方式来存储聊天记录, 对于重新拉取的源码， 可能会出现记忆缺失的情况, 对此, 请手动重命名memory下的chat_memory.json文件, 以便恢复记忆')
+logging.warning('10.14更新：由于协议库问题, 机器人登录可能失效。若您无法登录,请使用chatglm2-6b下的web_demo.sh运行本微调模型')
 
 try:
     #使用API模式调用模型
@@ -14,8 +15,13 @@ try:
         model = Model('model/chatglm2-6b','model/Muice')
     else:
         model = Model('model/chatglm2-6b-int4','model/Muice')
-except:
+except OSError:
     logging.error('模型加载失败, 请检查是否在model目录下放置了初始模型与微调模型')
+    traceback.print_exc()
+    exit()
+except:
+    logging.error('模型加载失败！相关报错信息如下:')
+    traceback.print_exc()
     exit()
 
 configs = json.load(open('configs.json','r',encoding='utf-8'))
