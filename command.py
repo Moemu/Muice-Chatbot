daebbef9ab3949af646549961e2fe245aca765f6
MoeSnowyFox <snowyfox_@outlook.com>
2024-06-02 00:58:35 +0800

    :recycle:整理代码, 消息处理并入onebot.py

--- command.py
+++ command.py
@@ -5,70 +5,38 @@ class Command():
     指令类
     '''
     def __init__(self,Muice):
-        self.commands = []
-        self.commands_function = []
         self.Muice = Muice
-        
-    def resister_command(self,command:str,command_function):
-        '''
-        注册命令
-        Args:
-            command: 命令字符串
-            command_function: 所执行的函数
-        '''
-        self.commands.append(command)
-        self.commands_function.append(command_function)
 
-    def run(self,command:str) -> str:
-        '''
-        执行命令，返回命令命令执行结果或命令不存在的提示
-        '''
-        for i in range(len(self.commands)):
-            if self.commands[i] == command:
-                return self.commands_function[i]()
-        return self.no_command()
-    
     def no_command(self):
         return "没有当前命令"
     #用于后期其他功能扩展
 
-    def load_default_command(self):
-        '''
-        加载默认命令
-        '''
-        self.resister_command('/help',self.default_help)
-        self.resister_command('/refresh',self.refresh)
-        self.resister_command('/clean',self.clean)
-        self.resister_command('/reset',self.reset)
-        self.resister_command('/undo',self.undo)
-        self.resister_command('/restart',self.restart)
-        
-    def default_help(self):
+    def default_help(self,mess_self):
         help_text = " /clean 清空本轮对话历史 \n /help 显示所有可用的命令列表 \n /refresh 刷新本次对话回复 \n /reset 重置所有对话数据(将存档对话数据) \n /undo 撤销上一次对话"
         return help_text
     
-    def refresh(self):
+    def refresh(self,mess_self):
         reply = self.Muice.refresh()
         self.Muice.save_chat_memory(reply)
         return reply
     
-    def clean(self):
+    def clean(self,mess_self):
         self.Muice.history = []
         return "cleaned"
     
-    def reset(self):
+    def reset(self,mess_self):
         shutil.copy(f'./memory/{self.Muice.user_qq}.json','./memory/chat_memory_backup.json')
         os.remove(f'./memory/{self.Muice.user_qq}.json')
         self.Muice.history = []
         return "reseted"
     
-    def undo(self):
+    def undo(self,mess_self):
         self.Muice.remove_last_chat_memory()
         self.Muice.history = self.Muice.get_recent_chat_memory()
         return "undoed"
     
     
-    def restart(self):
+    def restart(self,mess_self):
         print("restarting...")
         current_script = sys.argv[0]
         # 使用os.execv来替换当前进程并重新执行main.py
@@ -76,6 +44,10 @@ class Command():
         #os.execv(sys.executable, [sys.executable, current_script] + sys.argv[1:])
         #重启逻辑,或许会有
         return "restart"
+    
+    def echo(self,mess_self):
+        content = mess_self.mess[6:]
+        return content
 
     
     def execute(self, command: str) -> str:
--- onebot.py
+++ onebot.py
@@ -2,15 +2,22 @@ import logging
 from flask import Flask, request, Response
 import requests
 import time
-from command import Command
 from apscheduler.schedulers.background import BackgroundScheduler  
+
+from command import Command
+import onebot_def 
+
 
 class QQBotFlaskApp:
     def __init__(self, muice_app,configs):
         self.app = Flask(__name__)
         self.command = Command(muice_app) 
+        
 
-        self.command.load_default_command()
+
+        self.commands = []
+        self.commands_function = []
+        self.Muice = muice_app
         
 
         self.muice_app = muice_app
@@ -18,62 +25,96 @@ class QQBotFlaskApp:
         self.auto_create_topic = configs['AutoCreateTopic']
         self.send_post = configs['send_post']
         self.time_dict = {qq_id: time.time() for qq_id in self.trust_qq_list}
+        self.mess = ""
+        self.sender_id = ""
 
 
         self.scheduler = BackgroundScheduler()  
-        self.scheduler.add_job(self.timework, 'interval', minutes=1)  # 每分钟执行一次timework函数  
+        self.scheduler.add_job(self.timework, 'interval', minutes=23)  # 每23分钟执行一次timework函数  
         self.scheduler.start()
-        print(self.time_dict)
+        self.load_default_command()
         
 
         @self.app.route('/', methods=['POST'])
         def qqbot():
             data = request.json
-            sender_user_id = data.get('sender', {}).get('user_id')
-            mess = ' '.join([item['data']['text'] for item in data['message'] if item['type'] == 'text'])
-            logging.info(f'{sender_user_id} say: {mess}')
-            if data.get('message_type') == "private" and sender_user_id and 'message' in data and sender_user_id in self.trust_qq_list: 
-                self.reply_mess(mess, sender_user_id)
+            self.sender_id = data.get('sender', {}).get('user_id')
+            self.mess = ' '.join([item['data']['text'] for item in data['message'] if item['type'] == 'text'])
+            logging.info(f'{self.sender_id} say: {self.mess}')
+            if data.get('message_type') == "private" and self.sender_id and 'message' in data and self.sender_id in self.trust_qq_list: 
+                self.reply_mess()
                 return Response(status=200)
             return Response(status=404)
 
-
-    def reply_mess(self, mess, sender_user_id):
-        self.store_time(time.time(),sender_user_id)
-        if not str(mess).strip():
-                return None
-        if str(mess).startswith('/'):
-            reply = self.command.run(mess)
-            self.prmess(prid=sender_user_id, messages=reply)
-        else:
-            reply = self.muice_app.ask(text=mess, user_qq=sender_user_id)
-            for reply_item in reply:
-                time.sleep(len(reply_item)*0.8)
-                self.prmess(prid=sender_user_id, messages=reply_item)
-                
-            self.muice_app.finish_ask(str(reply))
-        return None    
-
-                
     def run(self, host='127.0.0.1', port=0000, debug=False):
         '''启动函数'''
         self.app.run(host=host, port=port, debug=debug)  
 
 
-    def prmess( self,  prid , messages ):
-        '''发送私聊消息'''
-        url = "http://127.0.0.1:"+str(self.send_post)+"/send_private_msg"                      
-        headers = {"content-type":"application/json",'Connection':'close'}
-        mess = {"user_id":prid,"message":messages}
-        res = requests.post(url, json = mess,headers=headers)
-        return(res.text)
-    
-    def store_time(self, time, id):  
+
+    def resister_command(self,command:str,command_function):
+        '''
+        注册命令
+        Args:
+            command: 命令字符串
+            command_function: 所执行的函数
+        '''
+        self.commands.append(command)
+        self.commands_function.append(command_function)
+
+
+    def load_default_command(self):
+        '''
+        加载默认命令
+        '''
+        self.resister_command('/help',self.command.default_help)
+        self.resister_command('/refresh',self.command.refresh)
+        self.resister_command('/clean',self.command.clean)
+        self.resister_command('/reset',self.command.reset)
+        self.resister_command('/undo',self.command.undo)
+        self.resister_command('/restart',self.command.restart)
+        self.resister_command('/echo',self.command.echo)  
+        self.resister_command('/Audio',self.Audio)  
+
+    def command_run(self,mess_self) -> str:
+        '''
+        执行命令，返回命令命令执行结果或命令不存在的提示
+        '''
+        parts = mess_self.mess.split(' ')
+    # 如果分割后有多于一个的部分，则返回第一个部分，否则返回原字符串
+        mess = parts[0] if len(parts) > 1 else mess_self.mess
+        for i in range(len(self.commands)):
+            if self.commands[i] == mess:
+                return self.commands_function[i](mess_self)
+        return self.command.no_command()
+
+
+
+
+
+    def reply_mess(self):
+        print(self.send_post)
+        if not str(self.mess).strip():
+                return None
+        if str(self.mess).startswith('/'):
+            reply = self.command_run(mess_self = self)
+            onebot_def.prmess(prid=self.sender_id, messages=reply, send_post=self.send_post)
+        else:
+            reply = self.muice_app.ask(text=self.mess, user_qq=self.sender_id)
+            for reply_item in reply:
+                time.sleep(len(reply_item)*0.8)
+                onebot_def.prmess(prid=self.sender_id, messages=reply_item, send_post=self.send_post)  
+                self.store_time()    
+            self.muice_app.finish_ask(str(reply))
+        return None    
+
+    def store_time(self):  
         """ 存储time_dict """  
+        id = self.sender_id  
         if id in self.time_dict:  
             # 如果id已存在，更新其time  
-            if self.time_dict[id] < time:  # 假设time是可以比较大小的（如datetime对象）  
-                self.time_dict[id] = time  
+            if self.time_dict[id] < time.time():
+                self.time_dict[id] = time.time()  
         else:  
             # 如果id不存在，添加新的键值对  
             self.time_dict[id] = time  
@@ -92,12 +133,14 @@ class QQBotFlaskApp:
                 mess = self.muice_app.ask(Topic, key)
                 self.reply_mess(mess, key)
                 return None
-
-
-
+            
     def __del__(self):  
-         if self.scheduler.running:  
+        if self.scheduler.running:  
             self.scheduler.shutdown()
+
+
+    def Audio(self,mess_self):   
+             
       
 
   
