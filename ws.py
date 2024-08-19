4da35a2d63dd437423c048383e43e1c64b212732
SnowyFox <156929726+MoeSnowyFox@users.noreply.github.com>
2024-08-19 11:17:41 +0800

    :sparkles:测试--群聊消息支持
    测试-支持回复群聊消息(历史记录仍有问题)
    在json文件中添加(非必要配置项)
        "Group_Message_Reply": true,
        "Trust_QQ_Group_list": [
            114514,
            191918
        ],
        "Group_Reply_Only_to_Trusted": true,
        "TEST_New_Group_Memory": true #未完成
    即可开启群聊消息功能

--- ws.py
+++ ws.py
@@ -25,6 +25,20 @@ async def build_reply_json(reply_message, sender_user_id):
     return json.dumps(data, ensure_ascii=False)
 
 
+async def build_group_reply_json(reply_message, group_id):
+    """构建回复的消息的json"""
+    if reply_message is None:
+        return None
+    data = {
+        "action": "send_group_msg",
+        "params": {
+            "group_id": group_id,
+            "message": reply_message
+        }
+    }
+    return json.dumps(data, ensure_ascii=False)
+
+
 class QQBot:
     def __init__(self, muice_app):
         self.app = FastAPI()
@@ -34,15 +48,25 @@ class QQBot:
         self.command.load_default_command()
 
         self.configs = json.load(open('configs.json', 'r', encoding='utf-8'))
-        self.trust_qq_list = self.configs['Trust_QQ_list']
-        self.websocket_port = self.configs['port']
-        self.auto_create_topic = self.configs['AutoCreateTopic']
+        self.trust_qq_list = self.configs.get('Trust_QQ_list', [])
+        self.websocket_port = self.configs.get('port')
+        # 主动对话
+        self.auto_create_topic = self.configs.get('AutoCreateTopic', False)
         if self.auto_create_topic:
             from apscheduler.schedulers.asyncio import AsyncIOScheduler
             self.time_dict = {qq_id: time.time() for qq_id in self.trust_qq_list}
             self.scheduler = AsyncIOScheduler()
             self.scheduler.add_job(self.time_work, 'interval', minutes=1)
             self.websocket = None
+        # 群聊消息
+        self.group_message_reply = self.configs.get('Group_Message_Reply', False)
+        if self.group_message_reply:
+            self.group_message_reply_list = self.configs.get('Trust_QQ_Group_list', [])
+            self.group_reply_only_to_trusted = self.configs.get('Group_Message_Reply_Only_To_Trusted', True)
+            self.TEST_New_Group_Memory = self.configs.get('TEST_New_Group_Memory', False)
+            logging.debug(f"group_message_reply_list = {self.group_message_reply_list}"
+                          f"group_reply_only_to_trusted = {self.group_reply_only_to_trusted}"
+                          f"TEST_New_Group_Memory = {self.TEST_New_Group_Memory}")
 
         @self.app.websocket("/ws/api")
         async def websocket_endpoint(websocket: WebSocket):
@@ -85,10 +109,30 @@ class QQBot:
             sender_user_id = data.get('sender', {}).get('user_id')
             message = ' '.join([item['data']['text'] for item in data['message'] if item['type'] == 'text'])
             logging.info(f"收到QQ{sender_user_id}的消息：{message}")
-            if sender_user_id in self.trust_qq_list:
-                reply_message = await self.produce_reply(message, sender_user_id)
-                reply = await build_reply_json(reply_message, sender_user_id)
-                return reply
+            if data['message_type'] == 'private':
+                logging.info(f"收到QQ{sender_user_id}的消息：{message}")
+                if sender_user_id in self.trust_qq_list:
+                    reply_message = await self.produce_reply(message, sender_user_id)
+                    reply = await build_reply_json(reply_message, sender_user_id)
+                    return reply
+
+            elif data['message_type'] == 'group' and self.group_message_reply:
+                group_id = data.get('group_id')
+                logging.info(f"收到群{group_id}QQ{sender_user_id}的消息：{message}")
+                if group_id in self.group_message_reply_list:
+                    if self.group_reply_only_to_trusted:
+                        if sender_user_id in self.trust_qq_list:
+                            reply_message = await self.produce_reply(message, sender_user_id)
+                            reply = await build_group_reply_json(reply_message, group_id)
+                            return reply
+                        else:
+                            return None
+                    else:
+                        reply_message = await self.produce_reply(message, sender_user_id)
+                        reply = await build_group_reply_json(reply_message, group_id)
+                        return reply
+                else:
+                    return None
             else:
                 return None
         else:
@@ -142,4 +186,3 @@ if __name__ == '__main__':
     Muice_app = None
     ws = QQBot(Muice_app)
     ws.run()
-
